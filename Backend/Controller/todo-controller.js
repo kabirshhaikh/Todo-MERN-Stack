91d459f13503c26918e5ebf10757a3d120179b21
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-04-05 01:09:18 -0400

    Implemented update functionality for both Put and Patch method

--- Backend/Controller/todo-controller.js
+++ Backend/Controller/todo-controller.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Task = require('../Model/Task');
-const { post, get } = require('../Routes/routes');
+const { post, get, search, put } = require('../Routes/routes');
 
 const testFunction = (req, res, next) => {
     res.status(201).send("Hello world from Todo Controller");
@@ -64,9 +64,108 @@ const getSingleTask = async (req, res, next) => {
 }
 
 //Todo Update task - also decide which method to use Patch or Put?
+//Implement Both
+
+const updatePatch = async (req, res, next) => {
+    const task = req.params.taskId;
+    const { taskName, taskDescription, taskPriority } = req.body;
+    console.log("Patch task:" + task);
+
+    let searchTask;
+
+    try {
+        searchTask = await Task.findOne({
+            where: {
+                taskId: task
+            }
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+    if (!searchTask) {
+        return res.status(400).json({ message: "Unable to find the task" });
+    }
+    else {
+        let patchUpdate;
+
+        try {
+            patchUpdate = await Task.update({
+                taskName: taskName == undefined || taskName == "" ? searchTask.taskName : taskName,
+                taskDescription: taskDescription == undefined || taskDescription == "" ? searchTask.taskDescription : taskDescription,
+                taskPriority: taskPriority == undefined || taskPriority == "" ? searchTask.taskPriority : taskPriority
+            }, {
+                where: {
+                    taskId: task
+                }
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+        if (!patchUpdate) {
+            return res.status(400).json({ message: "Unable to update task for the selected fields" });
+        }
+        else {
+            return res.status(201).json({ message: "Updated the task", result: patchUpdate });
+        }
+    }
+}
+
+
+const updatePut = async (req, res, next) => {
+    const { taskId } = req.params;
+    const { taskName, taskDescription, taskPriority } = req.body;
+    console.log("Task Id:" + taskId + "task name:" + taskName + "task desc:" + taskDescription + "task priority: " + taskPriority);
+
+    let foundTask;
+
+    try {
+        foundTask = await Task.findOne({
+            where: {
+                taskId: taskId
+            }
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+    if (!foundTask) {
+        return res.status(400).json({ message: "Unable to find the task" });
+    }
+    else {
+        let putUpdate;
+        try {
+            putUpdate = await Task.update({
+                taskName: taskName,
+                taskDescription: taskDescription,
+                taskPriority: taskPriority
+            }, {
+                where: {
+                    taskId: taskId
+                }
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+        if (!putUpdate) {
+            return res.status(400).json({ message: "Unable to update the task" });
+        }
+        else {
+            return res.status(201).json({ message: "Updated the task", result: putUpdate });
+        }
+    }
+}
 
 module.exports = {
     createTask,
     getAllTask,
-    getSingleTask
+    getSingleTask,
+    updatePatch,
+    updatePut
 }
--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -5,5 +5,7 @@ const todoController = require('../Controller/todo-controller');
 router.post('/create-task', todoController.createTask);
 router.get('/get-all-task', todoController.getAllTask);
 router.get('/get-single-task/:taskId', todoController.getSingleTask);
+router.patch('/update-task/:taskId', todoController.updatePatch);
+router.put('/update-task/:taskId', todoController.updatePut);
 
 module.exports = router;
