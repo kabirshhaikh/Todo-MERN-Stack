2e63147f86e0c95210bc6807a7c526a36f9344f7
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-04-08 03:10:43 -0400

    Created Task Middlewear and applied to all the required routes

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const todoController = require('../Controller/todo-controller');
 const userController = require('../Controller/user-controller');
 const userAuthenticaton = require('../Middlewear/user-middlewear');
+const taskAuthentication = require('../Middlewear/todo-middlewear');
 
 //User Routes:
 router.post('/create-user', userController.createUser);
@@ -12,9 +13,9 @@ router.get('/get-single-user/:userId', userController.getSingleUser);
 //Task Routes:
 router.post('/create-task/:userId', userAuthenticaton, todoController.createTask);
 router.get('/get-all-task', todoController.getAllTask);
-router.get('/get-single-task/:taskId', todoController.getSingleTask);
-router.patch('/update-task/:taskId', todoController.updatePatch);
-router.put('/update-task/:taskId', todoController.updatePut);
-router.delete('/delete-task/:taskId', todoController.deleteTask);
+router.get('/get-single-task/:userId/:taskId', taskAuthentication, todoController.getSingleTask);
+router.patch('/update-task/:userId/:taskId', taskAuthentication, todoController.updatePatch);
+router.put('/update-task/:userId/:taskId', taskAuthentication, todoController.updatePut);
+router.delete('/delete-task/:userId/:taskId', taskAuthentication, todoController.deleteTask);
 
 module.exports = router;
