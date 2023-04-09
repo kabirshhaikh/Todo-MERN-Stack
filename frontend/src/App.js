4183f15d3bbd49f2589fe0818cf44edcf8cd410e
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-04-09 04:02:23 -0400

    Implemented Register and Login Page, able to save a user in database from register page, in short register page is complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Register from './Components/Register';
+import Login from './Components/Login';
+
 
 function App() {
-  const [data, setData] = useState([]);
-
-  const fetchData = () => {
-    fetch('http://localhost:8000/get-all-task')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Unable to fetch the data");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      })
-      .catch(err => console.log(err));
-  };
-
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
     <div className="App">
-      {data.result.length > 0 && data.result.map((task) => {
-        return (
-          <div>
-            <h1>{task.taskId}</h1>
-            <h1>{task.taskName}</h1>
-            <h1>{task.taskDescription}</h1>
-            <h1>{task.taskPriority}</h1>
-          </div>
-        );
-      })}
+      <Routes>
+        <Route path='/' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
   );
 }
