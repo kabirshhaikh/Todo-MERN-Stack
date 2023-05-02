765668a9e0ec7e0f6cdddd42f86a8cac7dd56bea
Kabir Shaikh <shaikh.kab@northeastern.edu>
2023-05-01 23:27:30 -0400

    Used clever cloud to connect to the database to the cloud

--- Backend/Util/todoDatabase.js
+++ Backend/Util/todoDatabase.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.TABLENAME, process.env.USERNAME , process.env.PASSWORD, { dialect: 'mysql', host: process.env.HOST });
+const sequelize = new Sequelize('bfcsqqenowpthmlam2ty', 'uwwrowokua2uw6uh', 'wVocFA1R2xwM9SLlT59k', { dialect: 'mysql', host: 'bfcsqqenowpthmlam2ty-mysql.services.clever-cloud.com' });
 
 sequelize
     .authenticate()
